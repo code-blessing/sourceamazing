b29353a80c8ad039f341fb8ee4ab689f43267019
Jonathan Weiss <jonathan.weiss@codeblessing.org>
2023-12-20 13:28:40 +0100

    Remove unused methods (#10)

--- sourceamazing-api/src/main/kotlin/org/codeblessing/sourceamazing/api/process/schema/annotations/FacetType.kt
+++ sourceamazing-api/src/main/kotlin/org/codeblessing/sourceamazing/api/process/schema/annotations/FacetType.kt
@@ -1,8 +1,5 @@
 package org.codeblessing.sourceamazing.api.process.schema.annotations
 
-import org.codeblessing.sourceamazing.api.process.schema.ConceptIdentifier
-import kotlin.reflect.KClass
-
 enum class FacetType {
     TEXT,
     NUMBER,
@@ -10,33 +7,4 @@ enum class FacetType {
     REFERENCE,
     TEXT_ENUMERATION
     ;
-
-    fun isCompatibleInputType(facetValue: Any): Boolean {
-        return matchingEnumByInputFacetTypeClass(facetValue::class)
-            ?.let { matchingEnum -> matchingEnum == this }
-            ?: false
-    }
-
-    companion object {
-
-        fun matchingEnumByInputFacetTypeClass(classType: KClass<*>): FacetType? {
-            if(classType.java.isEnum) {
-                return TEXT_ENUMERATION
-            }
-            return when(classType) {
-                String::class -> TEXT
-                Int::class -> NUMBER // TODO is the cast to Int done properly in case of Long values?
-                Long::class -> NUMBER
-                Boolean::class -> BOOLEAN
-                ConceptIdentifier::class -> REFERENCE
-                else -> null
-            }
-        }
-
-        fun supportedTypes(): String {
-            return "${String::class.simpleName},${Int::class.simpleName},${Long::class.simpleName},${Boolean::class.simpleName},${ConceptIdentifier::class.qualifiedName},enum"
-        }
-    }
-
-
 }
