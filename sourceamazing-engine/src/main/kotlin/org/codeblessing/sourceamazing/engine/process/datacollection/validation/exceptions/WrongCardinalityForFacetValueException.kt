2ddd1f5010b71f40b19c475309942115f208b45a
Jonathan Weiss <jonathan.weiss@codeblessing.org>
2023-12-20 11:12:02 +0100

    Move exception message to where the exception is called to remove redundant information (#10)

--- sourceamazing-engine/src/main/kotlin/org/codeblessing/sourceamazing/engine/process/datacollection/validation/exceptions/WrongCardinalityForFacetValueException.kt
+++ sourceamazing-engine/src/main/kotlin/org/codeblessing/sourceamazing/engine/process/datacollection/validation/exceptions/WrongCardinalityForFacetValueException.kt
@@ -1,10 +1,4 @@
 package org.codeblessing.sourceamazing.engine.process.datacollection.validation.exceptions
 
-import org.codeblessing.sourceamazing.api.process.schema.ConceptIdentifier
-import org.codeblessing.sourceamazing.api.process.schema.ConceptName
-import org.codeblessing.sourceamazing.api.process.schema.FacetName
 
-
-class WrongCardinalityForFacetValueException(val concept: ConceptName, val conceptIdentifier: ConceptIdentifier, facetName: FacetName, reason: String): SchemaValidationException(
-    "Facet '$facetName' for concept identifier '${conceptIdentifier.name}' in concept '${concept}' has a wrong cardinality: $reason"
-)
+class WrongCardinalityForFacetValueException(msg: String): SchemaValidationException(msg)
