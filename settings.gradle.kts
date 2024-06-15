80f6af7d4311ade36d16e7df035a5505151af9ab
Jonathan Weiss <jonathan.weiss@codeblessing.org>
2024-06-15 16:06:49 +0200

    Update Junit 5 to version 5.10.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            val junit5Version = version("junit5", "5.9.1")
+            val junit5Version = version("junit5", "5.10.2")
             library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").versionRef(junit5Version)
 
             plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").version("1.9.24")
