777a4ce05c712e2a3c463dd4e74bd2c88bc82365
Jonathan Weiss <jonathan.weiss@codeblessing.org>
2024-12-16 11:37:26 +0100

    Small refactoring as preparation before unifying validation and data acces (#31) * return a map instead of a List of pairs * Remove unused class (replaced by KotlinInvocationHandler) * Remove shadowed it and replace it by function reference * Make some methods private if possible * Replace list of parameters with map having the KParameter as key

--- sourceamazing-schema/src/main/kotlin/org/codeblessing/sourceamazing/schema/schemacreator/query/proxy/SchemaInstanceInvocationHandler.kt
+++ sourceamazing-schema/src/main/kotlin/org/codeblessing/sourceamazing/schema/schemacreator/query/proxy/SchemaInstanceInvocationHandler.kt
@@ -7,6 +7,7 @@ import org.codeblessing.sourceamazing.schema.proxy.KotlinInvocationHandler
 import org.codeblessing.sourceamazing.schema.proxy.ProxyCreator
 import org.codeblessing.sourceamazing.schema.schemacreator.query.QueryMethodUtil
 import org.codeblessing.sourceamazing.schema.toConceptName
+import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.full.findAnnotation
 
@@ -15,7 +16,7 @@ class SchemaInstanceInvocationHandler(private val conceptGraph: ConceptGraph): K
     override fun invoke(proxy: Any, function: KFunction<*>, arguments: List<Any?>): Any? {
         function.findAnnotation<QueryConcepts>()?.let {
             val conceptClasses = it.conceptClasses
-            val conceptNamesToQuery = conceptClasses.map { it.toConceptName() }.toSet()
+            val conceptNamesToQuery = conceptClasses.map(KClass<*>::toConceptName).toSet()
             val conceptNodes = conceptGraph.conceptsByConceptNames(conceptNamesToQuery)
             val proxyList = conceptNodes.map { conceptNode ->
                 ProxyCreator.createProxy(
