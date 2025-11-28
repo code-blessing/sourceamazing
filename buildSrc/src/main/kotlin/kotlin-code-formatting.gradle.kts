85c23c1da8473b0a71277933980761be00260059
Jonathan Weiss <jonathan.weiss@codeblessing.org>
2025-11-28 08:54:26 +0100

    Set maxWidth for lines to 120 (before 100 default)

--- buildSrc/src/main/kotlin/kotlin-code-formatting.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-code-formatting.gradle.kts
@@ -7,6 +7,8 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         target("src/**/*.kt")
         // Use ktfmt which keeps params on one line if they fit the max width, otherwise wraps
         // See: https://github.com/facebook/ktfmt and Spotless plugin docs
-        ktfmt("0.59").kotlinlangStyle()
+        ktfmt("0.59").kotlinlangStyle().configure { config ->
+            config.setMaxWidth(120)
+        }
     }
 }
