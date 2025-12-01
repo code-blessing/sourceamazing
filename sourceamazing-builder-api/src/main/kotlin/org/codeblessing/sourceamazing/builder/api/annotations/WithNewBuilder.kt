ea777d771e4aa4f03a90a266813baa13ac8d795e
Jonathan Weiss <jonathan.weiss@codeblessing.org>
2025-12-01 13:45:43 +0100

    Remove unused @WithNewBuilder annotation

--- sourceamazing-builder-api/src/main/kotlin/org/codeblessing/sourceamazing/builder/api/annotations/WithNewBuilder.kt
+++
@@ -1,7 +0,0 @@
-package org.codeblessing.sourceamazing.builder.api.annotations
-
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class WithNewBuilder(val builderClass: KClass<*>)
